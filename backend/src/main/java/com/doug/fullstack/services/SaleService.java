fea6f1157efb78e20094a4d06845cb51ac8399f3
Doug <higoshi.douglas@gmail.com>
2021-05-06 13:29:24 -0300

    Busca paginada

--- backend/src/main/java/com/doug/fullstack/services/SaleService.java
+++ backend/src/main/java/com/doug/fullstack/services/SaleService.java
@@ -1,24 +1,45 @@
 package com.doug.fullstack.services;
 
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.doug.fullstack.DTO.SaleDTO;
+import com.doug.fullstack.DTO.SaleSuccessDTO;
+import com.doug.fullstack.DTO.SaleSumDTO;
 import com.doug.fullstack.entities.Sale;
 import com.doug.fullstack.repositories.SaleRepository;
+import com.doug.fullstack.repositories.SellerRepository;
 
 @RestController
 public class SaleService {
 	
 	@Autowired
 	private SaleRepository repository;
+	@Autowired
+	private SellerRepository sellerRepository;
 	
+	
+	@Transactional(readOnly = true)
 	public Page<SaleDTO> findAll(Pageable pageable){
+		sellerRepository.findAll();
 		Page<Sale> result =  repository.findAll(pageable);
 		return result.map(x -> new SaleDTO(x));
+	}
+	
+	@Transactional(readOnly = true)
+	public List<SaleSumDTO> amountGroupedBySeller(){
+		return repository.amountGroupedBySeller();
+	}
+	
+	@Transactional(readOnly = true)
+	public List<SaleSuccessDTO> successGroupedBySeller(){
+		return repository.successGroupedBySeller();
 	}
 	
 	

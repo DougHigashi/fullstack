689eafa84b0982e4ba66b58c1b0afd3c0e7d2e23
Doug <higoshi.douglas@gmail.com>
2021-05-09 00:08:36 -0300

    Navegação por rotas

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,10 +1,26 @@
+import Footer from "components/Footer";
+import NavBar from "components/NavBar";
+import { Link } from "react-router-dom";
 
 
 export default function Home() {
     return (
-        <div>
-            <h1>Home</h1>
-        </div>
+        <>
+            <NavBar />
+            <div className="container">
+                <div className="jumbotron">
+                    <h1 className="display-4">Fullstack</h1>
+                    <p className="lead">Analise o desempenho das suas vendas por diferentes perspectivas</p>
+                    <hr />
+                    <p>Esta aplicação consiste em exibir um dashboard a partir de dados fornecidos por um back end construído com Spring Boot.</p>
+
+                    <Link className="btn btn-primary btn-lg" to="/dashboard">
+                        Acessar o app
+                    </Link>
+                </div>
+            </div>
+            <Footer />
+        </>
 
     );
 }

0050324a648eba35712b9037da0ebdf4f7421de4
Doug <higoshi.douglas@gmail.com>
2021-05-09 19:30:02 -0300

    Gráfico de rosca integrado com backend

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { useEffect, useState } from 'react';
 import Chart from 'react-apexcharts'
 import { SaleSum } from 'types/sale';
 import { BASE_URL } from 'utils/requests';
@@ -11,24 +12,26 @@ type ChartData = {
 
 export default function DonutChart() {
 
-    let chartData: ChartData = {
+
+    const [chartData, setChartData] = useState<ChartData>({
         labels: [],
         series: []
-    };
+    });
 
-    axios.get(`${BASE_URL}/sales/amount-by-seller`)
-        .then(response => {
-            const data = response.data as SaleSum[];
-            const myLabels = data.map(x => x.sellerName);
-            const mySeries = data.map(x => x.sum);
+    useEffect(() => {
+        axios.get(`${BASE_URL}/sales/amount-by-seller`)
+            .then(response => {
+                const data = response.data as SaleSum[];
+                const myLabels = data.map(x => x.sellerName);
+                const mySeries = data.map(x => x.sum);
 
-            chartData = {
-                labels: myLabels,
-                series: mySeries
-            };
+                setChartData({
+                    labels: myLabels,
+                    series: mySeries
+                });
+            })
+    }, []);
 
-            console.log(response.data);
-        })
 
     /*
     const mockData = {
